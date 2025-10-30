546fe6d559583e949b16408da92c0588f7e33dd4
Min <choominxi2@gmail.com>
2025-10-30 19:14:21 +0800

    removed old packaging

--- orderProcessing/InventoryService.java
+++
@@ -1,8 +0,0 @@
-package orderProcessing;
-
-public interface InventoryService {
-    Product findProductById(String productId);
-    boolean reserveStock(String productId, int quantity);
-    void releaseStock(String productId, int quantity);
-    void commitStock(String productId, int quantity);
-}
